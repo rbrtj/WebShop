b2ea98b5ad7b7ff4f39de8acc2e6fb1755ab50fc
rbrtj <robertjaszczurek@icloud.com>
2022-04-25 11:02:00 +0200

    added login auth, user input validation, logout functionality and error handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,28 +1,90 @@
-const User = require('../models/user.model');
+const User = require("../models/user.model");
+const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
 
 function getSignup(req, res) {
-  res.render('customer/auth/signup');
+  res.render("customer/auth/signup");
 }
 
-async function signup(req, res){
-  const user = new User(req.body.email,
-     req.body.password,
-     req.body.fullname,
-     req.body.street,
-     req.body.postal,
-     req.body.city);
+async function signup(req, res, next) {
+  
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) || !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])
+  ) {
+    res.redirect("/signup");
+    return;
+  }
 
-  await user.signup();
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
 
-  res.redirect('/login');
+  
+
+  try {
+    const existsAlready = await user.existsAlready();
+
+  if(existsAlready) {
+    res.redirect('/signup');
+    return;
+  }
+    await user.signup();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.redirect("/login");
 }
 
 function getLogin(req, res) {
-  res.render('customer/auth/login');
+  res.render("customer/auth/login");
+}
+
+async function login(req, res, next) {
+  const user = new User(req.body.email, req.body.password);
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    return next(error);
+  }
+  if (!existingUser) {
+    res.redirect("/login");
+    return;
+  }
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    existingUser.password
+  );
+  if (!passwordIsCorrect) {
+    res.redirect("/login");
+    return;
+  }
+  authUtil.createUserSession(req, existingUser, function () {
+    res.redirect("/");
+  });
+}
+
+function logout(req, res) {
+  authUtil.deleteUserAuthSession(req);
+  res.redirect("/");
 }
 
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin,
   signup: signup,
+  login: login,
+  logout: logout,
 };
--- models/user.model.js
+++ models/user.model.js
@@ -1,29 +1,45 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
-
+const db = require("../data/database");
 
 class User {
-  constructor(email, password, fullname, street, postal, city){
-  this.email = email;
-  this.password = password;
-  this.name = fullname;
-  this.address = {
-    street: street,
-    postalCode: postal,
-    city: city
-   };
+  constructor(email, password, fullname, street, postal, city) {
+    this.email = email;
+    this.password = password;
+    this.name = fullname;
+    this.address = {
+      street: street,
+      postalCode: postal,
+      city: city,
+    };
   }
 
-  async signup(){
-   const hashedPassword = await bcrypt.hash(this.password, 12);
+  async getUserWithSameEmail() {
+    return db.getDb().collection("users").findOne({ email: this.email });
+  }
 
-    await db.getDb().collection('users').insertOne({
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async signup() {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+
+    await db.getDb().collection("users").insertOne({
       email: this.email,
       password: hashedPassword,
       name: this.name,
-      address: this.address
+      address: this.address,
     });
+  }
+
+  hasMatchingPassword(hashedPassword) {
+    return bcrypt.compare(this.password, hashedPassword);
   }
 }
 
