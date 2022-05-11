b5564644428f2e59c6978c35cfecd5d5c7924460
rbrtj <robertjaszczurek@icloud.com>
2022-05-11 13:40:32 +0200

    Added product model, storing product in db, updating product functionality

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,16 +1,62 @@
+const Product = require('../models/product.model');
+
 const res = require("express/lib/response");
 
-function getProducts(req, res){
-  res.render('admin/products/all-products');
+async function getProducts(req, res, next){
+  try{
+    const products = await Product.findAll();
+    res.render('admin/products/all-products', {products: products});
+  } catch(error){
+    next(error);
+    return;
+  }
 }
 
 function getNewProduct(req,res){
   res.render('admin/products/new-product')
 }
 
-function createNewProduct(req,res){
-  console.log(req.body);
-  console.log(req.file);
+async function createNewProduct(req,res, next){
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename
+  });
+
+  try{
+    await product.save();
+  } catch (error){
+    next(error);
+    return;
+  }
+
+  res.redirect('/admin/products');
+}
+
+async function getUpdateProduct(req, res, next) {
+  try{
+    const product = await Product.findById(req.params.id);
+    res.render('admin/products/update-product', { product: product });
+  } catch(error){
+    next(error);
+  }
+}
+
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id
+  });
+
+  if(req.file){
+    product.replaceImage(req.file.filename);
+  }  
+
+  try{
+    await product.save();
+  } catch(error) {
+    next(error);
+    return;
+  }
 
   res.redirect('/admin/products');
 }
@@ -18,5 +64,7 @@ function createNewProduct(req,res){
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
-  createNewProduct: createNewProduct 
+  createNewProduct: createNewProduct,
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct
 }
