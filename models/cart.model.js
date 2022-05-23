aa9a535ef41ad3ebb0af145aa31719d1bfdcc98e
rbrtj <robertjaszczurek@icloud.com>
2022-05-23 13:14:36 +0200

    Added cart page w styling, updating cart items

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,3 +1,5 @@
+const { text } = require("express");
+
 class Cart {
     constructor(items = [], totalQuantity = 0, totalPrice = 0) {
         this.items = items;
@@ -15,8 +17,8 @@ class Cart {
         for(let i = 0; i < this.items.length; i++){
             const item = this.items[i];
             if(item.product.id === product.id){
-                cartItem.quantity = cartItem.quantity + 1;
-                cartItem.totalPrice = cartItem.totalPrice + product.price;
+                cartItem.quantity = item.quantity + 1;
+                cartItem.totalPrice = item.totalPrice + product.price;
                 this.items[i] = cartItem;
 
                 this.totalQuantity++;
@@ -27,6 +29,31 @@ class Cart {
         this.items.push(cartItem);
         this.totalQuantity++;
         this.totalPrice += product.price;
+    }
+
+    updateItem(productId, newQuantity) {
+        for(let i = 0; i < this.items.length; i++){
+            const item = this.items[i];
+            if(item.product.id === productId && newQuantity > 0){
+                const cartItem = {...item};
+                const quantityChange = newQuantity - item.quantity;
+                cartItem.quantity = newQuantity
+                cartItem.quantity = item.quantity + 1;
+                cartItem.totalPrice = newQuantity * product.price;
+                this.items[i] = cartItem;
+
+                this.totalQuantity = this.totalQuantity + quantityChange;
+                this.totalPrice += quantityChange * product.price;
+                return { updatedItemPrice: cartItem.totalPrice };
+            } else if (item.product.id === productId && newQuantity <= 0){
+                this.items.splice(i, 1);
+                this.totalQuantity = this.totalQuantity - item.quantity;
+                this.totalPrice -= item.totalPrice;
+                return { updatedItemPrice: 0 };
+            }
+        }
+        
+        
     }
 }
 
